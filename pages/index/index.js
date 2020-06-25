0f151bb975caf4fd4edf2b469f95e3485680b8c1
Ryan1565222520 <62429908+Ryan1565222520@users.noreply.github.com>
2020-06-25 13:17:55 +0800

    first commit

--- pages/index/index.js
+++ pages/index/index.js
@@ -31,7 +31,8 @@ Page({
           latitude:res.latitude,
           longitude:res.longitude
         })
-        console.log(res);
+        //5.加载附近可用的10辆单车
+        findNearAll(that,that.data.latitude,that.data.longitude);
       }
     })
     //4.在地图页中加入按钮
@@ -106,37 +107,27 @@ Page({
       }
     })
 
-    //5.加载附近可用的10辆单车
-    wx.request({
-      url:"http://localhost:8080/ibike/findNearAll",
-      method:"POST",
-      data:{
-        latitude:that.data.latitude,
-        longitude:that.data.longitude,
-        status:1
-      },
-      success:function(res){
-        console.log("==="+res);
-        const bikes=res.data.obj.map(item=> {
-          return{
-            bid: item.bid,
-             iconPath:"../images/bike.png",
-             width:35,
-             height:35,
-             latitude:item.latitude,
-             longitude:item.longitude
-          }
-        });
-        console.log("===="+bikes)
-        that.setData({
-          markers:bikes
-        })
-      }
-    })
+    
         
       
     
 
+  },
+
+  //移动地图便能获取附近的车
+  regionchange(e){
+    var that=this
+    that.markers=[]
+    if(e.type=='end'){
+      that.mapCtx.getCenterLocation({
+        success:function(res){
+          var latitude=res.latitude;
+          var longitude=res.longitude;
+          //查找附近的车
+          findNearAll(that,latitude,longitude);
+        }
+      })
+    }
   },
 
   controltap(e) {
@@ -230,47 +221,32 @@ Page({
         }
       })
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
   }
 })
+function findNearAll(that,latitude,longitude){
+  //5.加载附近可用的10辆单车
+  wx.request({
+    url:"http://localhost:8080/ibike/findNearAll",
+    method:"POST",
+    data:{
+      latitude:latitude,
+      longitude:longitude,
+      status:1
+    },
+    success:function(res){
+      const bikes=res.data.obj.map(item=> {
+        return{
+          bid: item.bid,
+           iconPath:"../images/bike.png",
+           width:35,
+           height:35,
+           latitude:item.latitude,
+           longitude:item.longitude
+        }
+      });
+      that.setData({
+        markers:bikes
+      })
+    }
+  })
+}
